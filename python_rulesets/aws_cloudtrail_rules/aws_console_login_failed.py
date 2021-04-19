bdeb09abd863faf9b2a25f6943e6ab6ecb6468b5
bdutchsec <bouke@dutchsec.com>
2021-04-19 19:52:00 +0200

    fix imports

--- python_rulesets/aws_cloudtrail_rules/aws_console_login_failed.py
+++ python_rulesets/aws_cloudtrail_rules/aws_console_login_failed.py
@@ -1,5 +1,5 @@
 from python_rules import Rule, deep_get
-from test_rules_repo_master.python_rulesets.aws_cloudtrail_rules._mapping import original_get
+from test_rules_repo_small_master.python_rulesets.aws_cloudtrail_rules._mapping import original_get
 
 
 class ConsoleLoginFailed(Rule):
